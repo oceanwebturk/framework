1e85caf44b572ccedc1b604981d7e0c8f1e80120
Mehmet Ali Durusoy <mehmetalidurusoy73+github@gmail.com>
2024-01-27 12:14:23 +0300

    Update PackageManifest.php

--- src/PackageManifest.php
+++ src/PackageManifest.php
@@ -4,24 +4,35 @@ namespace OceanWT;
 
 class PackageManifest{
  
+ /**
+  * @return array
+  */
  public function providers(): array
  {
   return $this->config("providers");
  }
 
+ /**
+  * @param string $key
+  */
  public function config(string $key)
  {
-  return $this->getManifest()[$key];
+  return isset($this->getManifest()[$key]) ? $this->getManifest()[$key] : [];
  }
-
+ 
+ /**
+  * @return array
+  */
  public function getManifest(): array
  {
+  $return=[];
   $autoPackages=json_decode(file_get_contents(GET_DIRS['VENDOR']."composer/installed.json"),true)['packages'];
-  $packages=[json_decode(file_get_contents(REAL_BASE_DIR."composer.json"),true)]+$autoPackages;
+  $packages=array_merge([json_decode(file_get_contents(REAL_BASE_DIR."composer.json"),true)],$autoPackages);
   for ($i=0;$i<count($packages);$i++){ 
    if(isset($packages[$i]['extra']['oceanwebturk'])){
-   	return $packages[$i]['extra']['oceanwebturk'];
+    $return=array_merge($return,$packages[$i]['extra']['oceanwebturk']);
    }
   }
+  return $return;
  }
 }
